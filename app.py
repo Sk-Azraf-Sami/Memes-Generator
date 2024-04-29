6b5f7938963d6ef5819fa4e349eee22dfe67765d
Sk-Azraf-Sami <azraf7026official@gmail.com>
2024-04-29 23:03:23 +0600

    showing uploaded image

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'  # replace with your upload directory
+app.config['UPLOAD_FOLDER'] = 'static'
 
 @app.route('/', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+        return jsonify(success=False, message='No file part'), 400
 
     file = request.files['file']
 
     if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+        return jsonify(success=False, message='No selected file'), 400
 
     if file:
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        
-        # Ensure the directory exists
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        
         file.save(file_path)
-        return jsonify({'file_path': file_path}), 200
+        return jsonify(success=True, file_path=file_path), 200
 
 @app.route('/')
 def index():
