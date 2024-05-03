0887afb48a263f5400d32889becb68d2fae16882
Sk-Azraf-Sami <azraf7026official@gmail.com>
2024-05-03 11:56:35 +0600

    increase collage image size

--- collage.py
+++ collage.py
@@ -4,10 +4,12 @@ import os
 def collage(rows, cells, images_path):
     # List of lists of images
     images = [[os.path.join(images_path, f'{i}_{j}.jpg') for j in range(1, cells[i-1]+1)] for i in range(1, rows+1)]
+    
+    image_size_factor = 400
 
     # Determine the size of the collage
-    collage_height = len(images) * 200
-    collage_width = max(len(row) for row in images) * 200
+    collage_height = len(images) * image_size_factor
+    collage_width = max(len(row) for row in images) * image_size_factor
 
     # Create a new image for the collage
     collage = Image.new('RGB', (collage_width, collage_height), 'white')
@@ -17,11 +19,11 @@ def collage(rows, cells, images_path):
     for row in images:
         x = 0
         img_width = collage_width // len(row)
-        resized_imgs = [Image.open(i).resize((img_width, 200)) for i in row]
+        resized_imgs = [Image.open(i).resize((img_width, image_size_factor)) for i in row]
         for img in resized_imgs:
             collage.paste(img, (x, y))
             x += img_width
-        y += 200
+        y += image_size_factor
 
     # Save the collage
     meme_path = 'static/latest.jpg'
